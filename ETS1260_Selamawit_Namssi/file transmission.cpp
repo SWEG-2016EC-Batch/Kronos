a02c2d2391dd86b35477f6903b2c133211a598f5
selamawitN <selamawit.namssi@gmail.com>
2024-12-01 01:29:09 +0300

    Update file transmission by adding the code.cpp

--- ETS1260_Selamawit_Namssi/file transmission.cpp
+++ ETS1260_Selamawit_Namssi/file transmission.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int transmissionRate = 960;
+    int bytesPerCharacter = 1; // 1 character = 8 bit = 1 byte
+
+    long int fileSize;
+
+    cout << "Enter the file size in bytes: ";
+    cin >> fileSize;
+
+    long int totalCharacters = fileSize / bytesPerCharacter;
+
+    long int transmissionTime =(totalCharacters) / transmissionRate;
+
+    long int hours =(transmissionTime) / 3600;
+    long int minutes = (transmissionTime % 3600) / 60;
+    long int seconds = (transmissionTime) % 60;
+
+    cout << "Time to send the file: " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds." << endl;
+
+    return 0;
+}
 
