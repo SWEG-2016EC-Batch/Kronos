eae2767ebf62572bc7d9c3dbd39d8d93cf02ed7c
selamawitN <selamawit.namssi@gmail.com>
2024-11-30 15:54:26 +0300

    added the code for power.cpp

--- ETS1260_Selamawit_Namssi/power.cpp
+++ ETS1260_Selamawit_Namssi/power.cpp
@@ -1,1 +1,19 @@
+//This is a program that calculate the expression x^y where the user inputs both variables
+#include <iostream>
+#include <cmath>
+using namespace std;
+float x,y,result;
+int main()
+{
+    cout<<"Enter the base of the number"<<endl;
+    cin>>x;
+  
+    cout<<"By what power you want to raise the base of the number?"<<endl;
+    cin>>y;
+  
+      result = pow(x,y);
+      cout<<"The result of the number is: "<<result<<endl;
+
+return 0;
+}
 
