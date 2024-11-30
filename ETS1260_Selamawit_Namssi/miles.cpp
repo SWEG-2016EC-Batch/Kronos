e67cf9af6f1f9ba8286887c7274e89f93d35dc98
selamawitN <selamawit.namssi@gmail.com>
2024-12-01 01:30:49 +0300

    Update miles by adding the code.cpp

--- ETS1260_Selamawit_Namssi/miles.cpp
+++ ETS1260_Selamawit_Namssi/miles.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double tankcapacity;
+    double milespergallon;
+
+    cout << "Enter the capacity of the fuel tank in gallons: ";
+    cin >> tankcapacity;
+
+    cout << "Enter the miles per gallon the automobile can drive: ";
+    cin >> milespergallon;
+
+    double totaldistance = tankcapacity * milespergallon;
+    cout << "The automobile can be driven " << totaldistance << " miles without refueling." << endl;
+
+    return 0;
+}
 
