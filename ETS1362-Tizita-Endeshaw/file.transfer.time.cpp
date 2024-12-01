409d1f7408862764c829d41f562b7e54df3026ec
Tizita Endeshaw Yilma <tizita.endeshaw@aastustudent.edu.et>
2024-12-01 20:41:46 +0300

    file.transfer.time.cpp

--- ETS1362-Tizita-Endeshaw/file.transfer.time.cpp
+++ ETS1362-Tizita-Endeshaw/file.transfer.time.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
 
 //Declare variables
-int transmissinrate=960,filesize;
+int transmissionrate=960,filesize;
 double time_in_seconds;
 int time_in_minutes , time_in_hours;
 
@@ -16,13 +16,13 @@ cout<<"Enter the file size : ";
 cin>>filesize;
 
 //calculate the time it takes to transfer the file in second
-time_in_seconds=static_cast<double>(filesize)/transmissinrate;
+time_in_seconds=static_cast<double>(filesize)/transmissionrate;
 
 //converting the time in seconds in to hours and minutes
 time_in_minutes=static_cast<int>(ceil(time_in_seconds/60));
 time_in_hours=static_cast<int>(ceil(time_in_seconds/60));
 
-cout<<"The time takes to send the file size of " <<filesize<< " bytes with transmissin rate of "<<transmissinrate<<" character/second is "<<endl;
+cout<<"The time takes to send the file size of " <<filesize<< " bytes with transmission rate of "<<transmissionrate<<" character/second is "<<endl;
  cout<<"Seconds : "<<time_in_seconds<<" seconds"<<endl;
  cout<<"Miinutes : "<<time_in_minutes<<" minutes"<<endl;
  cout<<"Hours : "<<time_in_hours<<" hours"<<endl;
