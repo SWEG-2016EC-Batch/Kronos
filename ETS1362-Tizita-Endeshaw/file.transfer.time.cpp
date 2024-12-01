6a81ce6f49367bf018545cef321b544bec991182
Tizita Endeshaw Yilma <tizita.endeshaw@aastustudent.edu.et>
2024-12-01 06:50:54 +0300

    Add files via upload

---
+++ ETS1362-Tizita-Endeshaw/file.transfer.time.cpp
@@ -0,0 +1,30 @@
+/*A serial transmission line can transmit 960 characters a second. Write a program that will calculate how long
+it will take to send a file, given the file size. Test your program on a 400MB (419,430,400 byte) file which may
+take days.*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+
+//Declare variables
+int transmissinrate=960,filesize;
+double time_in_seconds;
+int time_in_minutes , time_in_hours;
+
+//recieve file size from the user
+cout<<"Enter the file size : ";
+cin>>filesize;
+
+//calculate the time it takes to transfer the file in second
+time_in_seconds=static_cast<double>(filesize)/transmissinrate;
+
+//converting the time in seconds in to hours and minutes
+time_in_minutes=static_cast<int>(ceil(time_in_seconds/60));
+time_in_hours=static_cast<int>(ceil(time_in_seconds/60));
+
+cout<<"The time takes to send the file size of " <<filesize<< " bytes with transmissin rate of "<<transmissinrate<<" character/second is "<<endl;
+ cout<<"Seconds : "<<time_in_seconds<<" seconds"<<endl;
+ cout<<"Miinutes : "<<time_in_minutes<<" minutes"<<endl;
+ cout<<"Hours : "<<time_in_hours<<" hours"<<endl;
+return 0;
+}
