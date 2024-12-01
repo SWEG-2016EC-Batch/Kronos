19a96d2d4cbb2e1444b4b592d5cb6b36e8182169
ornina dereje tefera <derejeornina@gmail.com>
2024-12-01 04:18:37 +0300

    Create exponent.cpp

---
+++ ETS1573-Ornina-Dereje/exponent.cpp
@@ -0,0 +1,21 @@
+//Write a program that find the result of the expression x^y where the value of X and Y are entered by the user.
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+
+//Declare variables
+double x,y,x_the_power_of_y;
+
+//Input x and y from the user
+cout<<"Enter the value of x : ";
+cin>>x;
+cout<<"Enter the value of y : ";
+cin>>y;
+
+//calculate x the power of y
+x_the_power_of_y=pow(x,y);
+
+cout<<" x the power of y is equal to "<< x_the_power_of_y<<endl;
+return 0;
+}
