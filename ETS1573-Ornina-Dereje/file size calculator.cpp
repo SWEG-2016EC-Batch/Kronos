a44d94d99a8ebc6bcf6d008a3f20ed4affe0a1de
ornina dereje tefera <derejeornina@gmail.com>
2024-12-01 04:38:05 +0300

    Create file size calculator.cpp

---
+++ ETS1573-Ornina-Dereje/file size calculator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int days,hours,minutes,seconds;
+    const int transmission_rate = 960; 
+
+    
+    long long file_size;
+    cout << "Enter the file size in bytes: ";
+    cin >> file_size;
+
+    
+    long long total_seconds = file_size / transmission_rate;
+
+    
+     days = total_seconds / (24 * 3600); 
+    total_seconds %= (24 * 3600); 
+    hours = total_seconds / 3600; 
+    total_seconds %= 3600; 
+    minutes = total_seconds / 60; 
+    seconds = total_seconds % 60;
+
+    
+    cout << "Transmission Time:" << endl;
+    cout << days << " days, " << hours << " hours, "<< minutes << " minutes, " << seconds << " seconds" << endl;
+
+    return 0;
+}
