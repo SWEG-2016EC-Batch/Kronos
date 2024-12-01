ba7dfc71fb1ed7068270b1dcf863cb5bf2fd6a7b
ornina dereje tefera <derejeornina@gmail.com>
2024-12-01 17:19:06 +0300

    Update BMI.cpp

--- ETS1573-Ornina-Dereje/BMI.cpp
+++ ETS1573-Ornina-Dereje/BMI.cpp
@@ -1,39 +1,38 @@
-//1. Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is in normal weight, under weight or over weight. BMI is calculated as (BMI = w/h*h). Make the program to work for multiple person before terminated
+/*Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
+in normal weight, under weight, over weight or obesity . BMI is calculated as (BMI = w/h*h). Make the program to
+work for multiple person before terminated.*/
+
 #include <iostream>
 using namespace std;
+int main (){
+    
+//Declare variables
+double height, weight, BMI;
+int number_of_people;
+int i,continue_;
+do{
+//Receive input(weight & height ) from the user
+cout<<"please enter your weight in kilograms: ";
+cin>> weight;
+cout<<"please enter yout height in meter  : ";
+cin>>height;
 
-int main() {
-   
-     double height,weight,BodyMassIndex;
-     int number_of_people;
+//calculate BMI
+BMI = weight/(height*height);
+cout<<"your BMI is : "<<BMI<<endl;
 
-cout<<" please enter no of people that wants to check BMI: ";
-cin>> number_of_people ;
-cout<<"here is the BMI of"<<number_of_people << " people"<<endl;
-for(int i=1;i<=number_of_people;i++){
-    cout<<"person no"<<i<<endl;
-   cout<<"please enter your height in meter: ";
-   cin>>height;
-   cout<<"please enter your weight in kilogram: ";
-   cin>>weight;
-   BodyMassIndex=weight/(height*height);
-   cout<<"your BodyMassIndex is: "<<BodyMassIndex<<endl;
-   if(BodyMassIndex<18.5 ){
-       cout<<"underweight"<<endl;
-   }
-       else if(BodyMassIndex>=25 && BodyMassIndex<=29.9 ){
-           cout<<"overweight"<<endl;
-       }
-           else if(BodyMassIndex>=18.5 && BodyMassIndex<=24.9){
-               cout<<"normal"<<endl;
-           }
-           else{
-           cout<<"please check your input"<<endl;
-           }
+//Determine whether the person is normal weight, under weight, over weight
+if(BMI < 18.5){
+    cout<<"you are under weight." <<endl;}
+else if(BMI>=18.5 && BMI<=24.9) {
+    cout<<"you are normal weight."<<endl;}
+else if(BMI>=25 && BMI <=29.9){
+    cout<<"you are over weight."<<endl;}
+
+else {cout<<"invalid input. "<<endl;}
+cout<<"Do you want to continue ? (if you want to continue write 1 and if you don't enter any number otherthan 1  ) : ";
+cin>>continue_;
+    }
+while(continue_==1);
+return 0;
 }
-    return 0;
-}
-    
-    
-    
-    
