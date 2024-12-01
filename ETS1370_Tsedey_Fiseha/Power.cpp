e98b92e62aeac17ea2a4c1cfa0e8bf18dfd3e19e
@tsedi16 <tsedey.fiseha@aastustudent.edu.et>
2024-12-01 07:27:15 +0300

    Update Power.cpp

--- ETS1370_Tsedey_Fiseha/Power.cpp
+++ ETS1370_Tsedey_Fiseha/Power.cpp
@@ -1,28 +1,24 @@
-# include <iostream>
+//3. Write a program that find the result of the expression 𝑥^𝑦 where the value of X and Y are entered by the user.
+
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
- long 
- double base_salary,bonus_rate,bonus,pension,tax,deductions,gross_salary,net_salary;
- int working_hours;
- string name;
- cout<<"enter your name"<<endl;
- cin>>name;
- cout<<"enter your base salary"<<endl;
- cin>>base_salary;
- cout<<"enter your weekly working hours"<<endl;
- cin>>working_hours;
- cout<<"enter your company bonus rate"<<endl;
- cin>>bonus_rate;
- bonus = bonus_rate * working_hours;
- gross_salary = base_salary + bonus; 
- pension = 0.05 * base_salary;
- tax = 0.15 * base_salary;
- net_salary = gross_salary -pension-tax ;
- cout<<"dear mr/s "<<name<<endl;
- cout<<"your bonus payment is :" <<bonus<<endl;
- cout<<"your gross salary is :" <<gross_salary<<endl;
-cout<<"your net salary is :"  <<net_salary<<endl; 
- 
-     
-return 0;
- }
+
+int main() {
+    double y, x, result;
+
+    
+    cout << "Enter the base of a number: ";
+    cin >> x;
+
+    cout << "Enter the power: ";
+    cin >> y;
+
+    
+    result = pow(x, y);
+
+    
+    cout << "The result is: " << result << endl;
+
+    return 0;
+}
