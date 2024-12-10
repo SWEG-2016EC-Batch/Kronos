06d7b8390e30b2d19829618951e4c2d1ade310ba
ornina dereje tefera <derejeornina@gmail.com>
2024-12-10 21:57:38 +0300

    Create picture2.cpp

---
+++ ETS1573-Ornina-Dereje/picture2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char character = 'A';
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j++){
+            cout<<" "<<character<<" ";
+            character++;
+        }
+        cout<<endl;
+    }
+ 
+    return 0;
+}
