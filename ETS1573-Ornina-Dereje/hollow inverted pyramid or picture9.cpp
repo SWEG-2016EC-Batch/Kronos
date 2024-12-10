161cd7cfd2349c601d7f3c6f4a25065b435eb8e7
ornina dereje tefera <derejeornina@gmail.com>
2024-12-10 20:45:04 +0300

    Create hollow inverted pyramid or picture9.cpp

---
+++ ETS1573-Ornina-Dereje/hollow inverted pyramid or picture9.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+int main() {
+for (int i=6;i>=1;i--){
+
+for(int j=1;j<=i;j++){ 
+    if(i==6 ||i==1 || j==i ||j==1){
+        cout<<"*";
+    }
+  else{
+      cout<<" ";
+  }
+}
+
+cout<<endl;
+}
+return 0;
+}
