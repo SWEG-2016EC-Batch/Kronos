c8fdb1519769f4e90fbe0d40b5952db7d8b34985
ornina dereje tefera <derejeornina@gmail.com>
2024-12-11 12:55:37 +0300

    Create 4bpicture4.cpp

---
+++ ETS1573-Ornina-Dereje/4bpicture4.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+int main() {
+      int n;
+    
+    cout<<"please enter number of n:";
+    cin>>n;
+   
+   for(int i=1;i<=n;i++){
+       for(int h=n-i;h>0;h--)
+       cout<<" ";
+       for(int j=i;j>=1;j--){
+           cout<<j;
+       }
+       cout<<endl;
+   }
+ 
+ 
+
+    return 0;
+}
