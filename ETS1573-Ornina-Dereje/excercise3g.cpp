97eed2b7b40c12d7568cb2b068a35d33372b8cc2
ornina dereje tefera <derejeornina@gmail.com>
2024-12-12 15:08:48 +0300

    Create excercise3g.cpp

---
+++ ETS1573-Ornina-Dereje/excercise3g.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number,reverse=0;
+    cout<<"Enter a number: ";
+    cin>>number;
+    int original =number;
+    while(number!=0){
+        int digit = number%10;
+        reverse = reverse*10 + digit;
+        number = number/10;
+
+    }
+    if(original==reverse){
+        cout<<"it is palindrome number";
+
+    }
+    else{
+        cout<<"it is not palindrome number";
+    }
+    return 0;
+}
