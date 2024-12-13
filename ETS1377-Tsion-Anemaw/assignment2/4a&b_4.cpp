8eed745fd1864d67fd79a56f22c1977335a31a7a
TsionA363 <atsion989@gmail.com>
2024-12-13 14:48:49 +0300

    Update 4a&b_4.cpp

--- ETS1377-Tsion-Anemaw/assignment2/4a&b_4.cpp
+++ ETS1377-Tsion-Anemaw/assignment2/4a&b_4.cpp
@@ -1,18 +1,20 @@
 //4a
 #include <iostream>
 using namespace std;
-int main() {
-   
-   for(int i=1;i<=5;i++){
-       for(int h=5-i;h>0;h--)
-       cout<<" ";
-       for(int j=i;j>=1;j--){
-           cout<<j;
-       }
-       cout<<endl;
-   }
- 
- 
+
+int main(){
+    for (int i = 1; i <= 5; i++) {
+        
+        for (int k = 1; k <= (5 - i) * 2; k++) {
+            cout << " ";
+        }
+       
+        for (int j = i; j >= 1; j--) {
+            cout << j << " ";
+        }
+
+        cout << endl; 
+    }
 
     return 0;
 }
@@ -22,22 +24,20 @@ using namespace std;
 
 int main() {
     int rows;
-
-    
-    cout << "Enter the number of rows: ";
+    cout << "Enter the number of rows for the pattern: ";
     cin >> rows;
 
-
+   
     for (int i = 1; i <= rows; i++) {
         
-        for (int h = rows - i; h > 0; h--) {
+        for (int k= 1; k<= (rows - i) * 2; k++) {
             cout << " ";
         }
-        
+
         for (int j = i; j >= 1; j--) {
-            cout << j;
+            cout << j << " ";
         }
-        
+
         cout << endl;
     }
 
