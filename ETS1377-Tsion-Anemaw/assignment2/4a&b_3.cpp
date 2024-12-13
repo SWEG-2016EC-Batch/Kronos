e6dce95b1f15340c295e44c476ed5644d319530e
TsionA363 <atsion989@gmail.com>
2024-12-13 03:05:49 -0800

    Update 4a&b_3.cpp

--- ETS1377-Tsion-Anemaw/assignment2/4a&b_3.cpp
+++ ETS1377-Tsion-Anemaw/assignment2/4a&b_3.cpp
@@ -1,3 +1,4 @@
+//4a
 #include <iostream>
 using namespace std;
 int main(){
@@ -7,5 +8,26 @@ int main(){
         }
         cout<<endl;
     }
+    return 0;
+}
+//4b
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    
+    for (int i = 1; i <= rows; i++) {
+        
+        for (int j = 1; j <= i; j++) {
+            cout << j << " ";
+        }
+        cout << endl; 
+    }
+
     return 0;
 }
