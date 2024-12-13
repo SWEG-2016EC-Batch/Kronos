a8b06fd92db172aa15396be862fad89cf46d3a0a
TsionA363 <atsion989@gmail.com>
2024-12-13 05:44:39 -0800

    Update 4a&b_4.cpp

--- ETS1377-Tsion-Anemaw/assignment2/4a&b_4.cpp
+++ ETS1377-Tsion-Anemaw/assignment2/4a&b_4.cpp
@@ -1,45 +1,40 @@
 //4a
 #include <iostream>
 using namespace std;
-
-int main(){
-    for (int i = 1; i <= 5; i++) {
-        
-        for (int k = 1; k <= (5 - i) * 2; k++) {
-            cout << " ";
-        }
-       
-        for (int j = i; j >= 1; j--) {
-            cout << j << " ";
-        }
-
-        cout << endl; 
-    }
+int main() {
+   
+   for(int i=1;i<=5;i++){
+       for(int h=5-i;h>0;h--)
+       cout<<" ";
+       for(int j=i;j>=1;j--){
+           cout<<j;
+       }
+       cout<<endl;
+   }
+ 
+ 
 
     return 0;
 }
 //4b
 #include <iostream>
 using namespace std;
-
 int main() {
-    int rows;
-    cout << "Enter the number of rows for the pattern: ";
-    cin >> rows;
-
+      int n;
+    
+    cout<<"please enter number of n:";
+    cin>>n;
    
-    for (int i = 1; i <= rows; i++) {
-        
-        for (int k= 1; k<= (rows - i) * 2; k++) {
-            cout << " ";
-        }
-
-        for (int j = i; j >= 1; j--) {
-            cout << j << " ";
-        }
-
-        cout << endl;
-    }
+   for(int i=1;i<=n;i++){
+       for(int h=n-i;h>0;h--)
+       cout<<" ";
+       for(int j=i;j>=1;j--){
+           cout<<j;
+       }
+       cout<<endl;
+   }
+ 
+ 
 
     return 0;
 }
